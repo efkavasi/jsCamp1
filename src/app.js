3469e743d70fa755558437514285861350e22076
efkavasi <eng_sen@shiftdelete.net>
2021-10-23 20:44:31 +0300

    18 ekim ders sonu

---
+++ src/app.js
@@ -0,0 +1,17 @@
+let dolarDun=9.20
+let dolarBugun=9.30
+
+console.log(dolarDun)
+
+const euroDun=11
+console.log(euroDun) 
+
+let konutKredileri=["Konut Kredisi","Emlak Kredisi","Kamu Konut Kredisi"]
+
+console.log("<ul>")
+
+for (let i=0;i<3;i++ ) {
+    
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
